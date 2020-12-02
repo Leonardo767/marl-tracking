f5cd5795119fd61bb0162d54af99be891d10fef0
Leonardo Pinero-Perez <leonardo.pinero42@gmail.com>
2020-12-02 01:44:49 -0600

    brought multiagent env into repo

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Muti-Agent RL Tracking
 
 Installation:
-* `git clone git@github.com:Leonardo767/marl-tracking.git`
-* As a separate cloned repo, `git clone https://github.com/openai/multiagent-particle-envs.git`
-* `cd` into the repo
+* `cd particle_env`
 * `pip install -e .`
+* `cd ..`
--- main.py
+++ main.py
@@ -1,17 +1,6 @@
-from multiagent.environment import MultiAgentEnv
-import multiagent.scenarios as scenarios
+from particle_env.make_env import make_env
+import time
 
-
-benchmark = False
-scenario_name = 'simple'
-# load scenario from script
-scenario = scenarios.load(scenario_name + ".py").Scenario()
-# create world
-world = scenario.make_world()
-# create multiagent environment
-if benchmark:
-    env = MultiAgentEnv(world, scenario.reset_world, scenario.reward,
-                        scenario.observation, scenario.benchmark_data)
-else:
-    env = MultiAgentEnv(world, scenario.reset_world,
-                        scenario.reward, scenario.observation)
+env = make_env('simple')
+env.render()
+time.sleep(0.5)
